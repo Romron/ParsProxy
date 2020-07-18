0e2433f229878c03447f38b8c86a417a8500c248
Romron <romron@ukr.net>
2020-07-19 00:36:44 +0300

    Добавил исключения для корректной обработки ситуации вкоторой URL недоступен, а списка прокси нет

---
+++ test 2.py
@@ -0,0 +1,15 @@
+
+result_listProxy = []
+
+try:
+	if len(result_listProxy) == 0:
+		raise NameError('NameError')
+
+		print('try')
+except NameError as e:
+	print('Ошибка')
+	# print(e)
+
+
+# print(len(result_listProxy))
+	
