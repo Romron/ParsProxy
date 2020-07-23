8273cee3906840bc66fee1845b469aae71a919b8
Romron <romron@ukr.net>
2020-07-23 16:22:35 +0300

    в test 3.py вариант получения страниц, до 5 включительно, нажатием на "следующая"

--- test 3.py
+++ test 3.py
@@ -5,33 +5,36 @@ import time
 from selenium.webdriver.common.keys import Keys
 import tkinter                 #  библиотека для графических интерфейсов 
 
+import numpy as np
+import scipy.interpolate as si
+from selenium.webdriver.common.action_chains import ActionChains
+
+
 r = tkinter.Tk()
 pathDriver = os.path.dirname(os.path.abspath(__file__)) + "/geckodriver.exe"
 opts = Options()
-
-
-# opts.add_argument('-width=600')
-# opts.add_argument('-height=600')
-
 opts.add_argument('-width=' + str(r.winfo_screenwidth()/2))
 opts.add_argument('-height=' + str(r.winfo_screenheight()/1.3))
-
 
 driver = webdriver.Firefox(executable_path=pathDriver,options=opts)
 driver.set_window_position(r.winfo_screenwidth()/2, 0)
 
-# driver.execute_script("win = window.open(" + test_IP_URL + ")")
-driver.execute_script("window.open()")
-
-# driver.get('https://2ip.ru/')
-
-two_Window = driver.window_handles[1]  2600:3c01::f03c:91ff:fec7:d371
-
-driver.switch_to.window(two_Window)
-driver.get('https://google.com')
+URL = 'http://free-proxy.cz/en/'
 
 
-time.sleep(5)
-driver.execute_script("window.close()")
 
-print('*******************')
+
+driver.get(URL)
+
+for x in range(4,15):
+	startElement = driver.find_element_by_xpath("/html/body/div[2]/div[1]/div[1]/ul/li[1]/a")
+	finishElement = driver.find_element_by_css_selector('div.paginator:nth-child(5) > a:nth-child(1' + str(x) + ')')
+	action = ActionChains(driver);
+	action.move_to_element(startElement);
+	time.sleep(2)
+	action.move_to_element(finishElement);
+	time.sleep(2)
+	action.click(finishElement)
+	action.perform();
+	print(x)
+	time.sleep(2)
